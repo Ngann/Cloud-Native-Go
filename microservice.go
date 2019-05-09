b9e511311ee89b2238b6fbaa1ba835b8a7ec2bbb
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-05-09 00:15:31 -0700

    implement api echo function and set header for message received

--- microservice.go
+++ microservice.go
@@ -8,6 +8,7 @@ import (
 
 func main(){
 	http.HandleFunc("/", index)
+	http.HandleFunc("api/echo", echo)
 	http.ListenAndServe(port(), nil)
 
 }
@@ -32,3 +33,15 @@ func port() string {
 	return ":" + port
 }
 
+
+//implement echo function
+// postman http://localhost:9090/api/echo?message=Cloud+Native+Go
+// response "Hello Cloud Native Go"
+// click header tab, notice the content type = text/plain
+func echo(w http.ResponseWriter, r *http.Request){
+	//extract the query parameter from the url and grab the first message
+	message := r.URL.Query()["message"][0]
+
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
+}
