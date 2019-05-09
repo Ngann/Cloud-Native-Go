2395826aee1b1e28d9e0bdb87ceca239d0f0d3d1
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-05-09 11:26:03 -0700

    add test and book function

--- api/book.go
+++ api/book.go
@@ -2,22 +2,34 @@ package api
 
 import (
 	"encoding/json"
+	_"encoding/json"
 	"net/http"
 	)
 
 // Book type with Name, Author, ISBN
 type Book struct {
-	//define the book
+	Title string
+	Author string
+	ISBN string
 }
 
-//toJson to be used for marshalling of Book type
+// ToJSON to be used for marshalling of Book type
 func (b Book) ToJSON() []byte {
-	return nil
+	ToJSON, err := json.Marshal(b)
+	if err != nil {
+		panic(err)
+	}
+	return ToJSON
 }
 
-//FromJson to be used for unmarshalling of Book Type
-func FromJson(data []byte) Book {
-
+// FromJSON to be used for unmarshalling of Book type
+func FromJSON(data []byte) Book {
+	book := Book{}
+	err := json.Unmarshal(data, &book)
+	if err != nil {
+		panic(err)
+	}
+	return book
 }
 
 //BookHandleFunc to be used as http.HandleFunc fro Book API
