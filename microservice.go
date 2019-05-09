f8dc6916e2cc9747494af6f88ba579d8291a7cc3
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-05-09 00:05:58 -0700

    extract port parameter to make it configurable

--- microservice.go
+++ microservice.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main(){
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port(), nil)
 
 }
 
@@ -16,8 +17,18 @@ func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello Cloud Native Go")
 }
 
+//extract port parameter to make it configurable
+// 8080 is still default
 
-
+// In terminal $ export PORT=9090
 //go run microservice.go
-// go to postman http://localhost:8080/ , send a get request
+// go to postman http://localhost:9090/ , send a get request
 // response should be " Hello Cloud Native"
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
+
